1d66c68b6be4c319c2bd38f270540c9146dc50cd
Felipe <feliperkj805@gmail.com>
2025-04-25 16:40:04 -0400

    Added Switch for actions, changed loop variable to use the built-in system one, used the connection to send mock data to the socket, and received data from the socket

--- RLPlayer.cs
+++ RLPlayer.cs
@@ -1,5 +1,6 @@
-
 using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -7,22 +8,49 @@ namespace RL_API
 {
     class RLPlayer() : ModPlayer
     {
-        
 
 		Tile_Scan tileScanner = new();
         List<object> tiles;
-		int loopTicks = 0;
+        int hertz = 1;
+        private static string lastKnownAction = "none";
         public override void SetControls()
 		{
-			// Example: Simulate moving left
-			Player.controlLeft = true;
+            string action = ConnectionManager.PeekAction()?.Trim();
+    
+            if (Main.GameUpdateCount % hertz == 0)
+            {
+                ModContent.GetInstance<RL_API>().Logger.Info("Updated Action: "+action);
 
-			// Example: Simulate jumping
-			if (Player.velocity.Y == 0f) // Ensure the player is grounded
-			{
-				Player.controlJump = true;
-			}
-		}
+                if (!string.IsNullOrEmpty(action))
+                    lastKnownAction = action;
+            }
+
+            switch (lastKnownAction)
+            {
+                case "left":
+                    Player.controlLeft = true;
+                    break;
+                case "right":
+                    Player.controlRight = true;
+                    break;
+                case "jump":
+                    Player.controlJump = true;
+                    break;
+                case "use_item":
+                    Player.controlUseItem = true;
+                    break;
+                case "none":
+                case null:
+                case "":
+                    // Do nothing
+                    break;
+                default:
+                    // Unknown action, maybe log?
+                    Main.NewText($"Unknown RL action: {lastKnownAction}");
+                    break;
+
+            }
+        }
 
         public override void PreUpdateBuffs()
         {
@@ -41,13 +69,18 @@ namespace RL_API
 		public override void PreUpdate()
     	{
 			//base.PreUpdate();
-			loopTicks++;
-			if(loopTicks % 5 == 0){
+			if(Main.GameUpdateCount % hertz == 0){
 				tiles = tileScanner.scanTiles(Main.LocalPlayer,3);
 			}
 		}
 		public override void PostUpdate()
 		{
+            if(Main.GameUpdateCount % hertz != 0) return;
+            //var data = "{obs: test}";
+            var obsJson = JsonSerializer.Serialize(new { obs = "test" });
+            ConnectionManager.EnqueueObservation(obsJson);  // just queue, no await/read here
+
+            var action = ConnectionManager.ConsumeAction();
 			//base.PostUpdate();
 			/*if(loopTicks % 60 == 0){
 				Main.NewText("Printing... " + loopTicks/60);
