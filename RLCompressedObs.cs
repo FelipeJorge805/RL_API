5c8c4dae4507b50168605f125107d65c0732c8c4
Felipe <feliperkj805@gmail.com>
2025-04-25 18:11:41 -0400

    Added namespace

--- RLCompressedObs.cs
+++ RLCompressedObs.cs
@@ -1,16 +1,19 @@
-public class RLCompressedObs
+namespace RL_API
 {
-    public float[] PlayerInfo { get; set; }    // [VelocityX, VelocityY, PosX, PosY, HealthRatio, ManaRatio]
-    public byte[] TileTypes { get; set; }       // Flattened 1D array of tile types around the player
-    public byte HeldItemType { get; set; }
-    public byte NearbyEnemies { get; set; }
-    public sbyte FacingDirection { get; set; }  // -1 = left, 1 = right
-    public byte TotalDefense { get; set; }
-    public byte[] BuffIds { get; set; }
-    public byte[] AccessoryIds { get; set; }
-    public byte CurrentBiome { get; set; } 
-    public byte HookType { get; set; }
-    public byte MountType { get; set; }
-    public byte LightPetType { get; set; }
-    public byte PetType { get; set; }     // Single biome ID (for now, or most important one)
+    public class RLCompressedObs
+    {
+        public float[] PlayerInfo { get; set; }    // [VelocityX, VelocityY, PosX, PosY, HealthRatio, ManaRatio]
+        public byte[] TileTypes { get; set; }       // Flattened 1D array of tile types around the player
+        public byte HeldItemType { get; set; }
+        public byte NearbyEnemies { get; set; }
+        public sbyte FacingDirection { get; set; }  // -1 = left, 1 = right
+        public byte TotalDefense { get; set; }
+        public byte[] BuffIds { get; set; }
+        public byte[] AccessoryIds { get; set; }
+        public byte CurrentBiome { get; set; } 
+        public byte HookType { get; set; }
+        public byte MountType { get; set; }
+        public byte LightPetType { get; set; }
+        public byte PetType { get; set; }     // Single biome ID (for now, or most important one)
+    }
 }
