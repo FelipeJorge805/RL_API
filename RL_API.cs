a0ca614828d825d3bd1b7991cb0582b13c2fc619
Felipe <feliperkj805@gmail.com>
2025-04-25 12:09:56 -0400

    Refactored RL_API to be the main file for load() and initialization

--- RL_API.cs
+++ RL_API.cs
@@ -11,56 +11,15 @@ using Terraria.ModLoader;
 namespace RL_API
 {
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class RL_API : ModPlayer
+	public class RL_API : Mod
 	{
-		Vector2 playerPos = Main.LocalPlayer.position;
-		List<object> tiles;
-		int loopTicks = 0;
-		Tile_Scan tileScanner = new();
-
+		public override void Load()
+        {
+        	ConnectionManager.Initialize("127.0.0.1", 5000);
+        }
 		/*public void saveLog(){
 			Logger.Info(playerPos);
 			ModContent.GetInstance<RL_API>().Logger.Info("This is a log message.");
 		}*/
-		
-		
-		public override void SetControls()
-		{
-			// Example: Simulate moving left
-			Player.controlLeft = true;
-
-			// Example: Simulate jumping
-			if (Player.velocity.Y == 0f) // Ensure the player is grounded
-			{
-				Player.controlJump = true;
-			}
-		}
-		
-		public override void PreUpdate()
-    	{
-			loopTicks++;
-			if(loopTicks % 5 == 0){
-				tiles = tileScanner.scanTiles(Main.LocalPlayer,3);
-				
-				//Player.controlRight = true;
-				//Player.controlLeft = true;
-				//Player.velocity.X = -2f;
-				//Player.velocity.Y = -10f;
-			}
-			//Player.controlJump = true;
-			
-			/*if(loopTicks % 60 == 0){
-				Main.NewText("Printing... " + loopTicks/60);
-				String t = "";
-				tiles.ForEach(o => t+=o+",");
-				Main.NewText(t);
-			}*/
-		}
-		public override void PostUpdate()
-		{
-			//Player.controlJump = true;
-			//Player.controlLeft = true;
-			//Player.velocity.X = -2f;
-		}
 	}
 }
