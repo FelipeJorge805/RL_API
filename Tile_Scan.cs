9c3891f53df62620f0d84ba8c5b4a07f6af89fe1
Felipe <feliperkj805@gmail.com>
2025-04-26 15:49:13 -0400

    spacing

--- Tile_Scan.cs
+++ Tile_Scan.cs
@@ -13,7 +13,7 @@ namespace RL_API
 		public static List<TileInfo> scanTiles(Player player, int radius){
 
             //int tileSize = 16;
-            int px = (int)(player.Center.X /16f);
+            int px = (int)(player.Center.X / 16f);
             int py = (int)(player.Center.Y / 16f);
 
             //var tileGrid = new List<List<object>>();
@@ -28,36 +28,36 @@ namespace RL_API
                         int tx = px + x;
                         int ty = py + y;
                         //ModContent.GetInstance<RL_API>().Logger.Info("tx:"+tx+" ty:"+ty + " px:"+px + " py:"+py + " x:"+x + " y:"+y);
-                        Tile tile = Main.tile[tx, ty];
-
-                        int tileType = tile.HasTile ? tile.TileType : -1;
-                        int liquid = tile.LiquidType;
-                        int liquidAmount = tile.LiquidAmount;
-
-                        //row.Add(new object[] { tileType, liquid, liquidAmount });
-                        tiles.Add(new TileInfo(tileType,liquid,liquidAmount));
+                        TileInfo tile = EncodeTile(tx,ty);
+                        row.Add(tile);
                     }
-                    //tileGrid.Add(row);
                 }
             }
             catch(Exception e){
                 ModContent.GetInstance<RL_API>().Logger.Info("Error in scan loop.");
                 ModContent.GetInstance<RL_API>().Logger.Error(e.Message);
             }
-            
-            
-            /*try{// Save to JSON file that Python can read
-                string path = "C:\\Users\\felip\\OneDrive\\Documents\\My Games\\Terraria\\tModLoader\\Mods";
-                File.WriteAllText(path, JsonSerializer.Serialize(tiles));
-
-                ModContent.GetInstance<Tile_Scan>().Logger.Info("Tile scan saved to RLTiles.json");
-            }
-            catch(Exception e){
-                ModContent.GetInstance<Tile_Scan>().Logger.Info("Error in writing to file");
-                ModContent.GetInstance<Tile_Scan>().Logger.Error(e.Message);
-            }*/
-
-            return null; 
+            return tiles; 
 		}
+        private static TileInfo EncodeTile(int tx, int ty)
+        {
+            float brightness = Lighting.Brightness(tx, ty);
+
+            if (brightness >= 0.2f)
+            {
+                Tile tile = Main.tile[tx, ty];
+                int tileType = tile.HasTile ? tile.TileType : -1;
+                tileType += 2;
+                int liquidType = tile.LiquidType + 1;
+                int liquidAmount = tile.LiquidAmount + 1;
+
+                return new TileInfo(tileType, liquidType, liquidAmount, brightness);
+            }
+            else
+            {
+                return new TileInfo(0, 0, 0, 0);
+            }
+        }
+
 	}
 }
