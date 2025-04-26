69405c3f90d2f54fda058fbbcdf8b7d4bd328ad8
Felipe <feliperkj805@gmail.com>
2025-04-26 15:49:33 -0400

    Added brightness field and added getter

--- TileInfo.cs
+++ TileInfo.cs
@@ -1,11 +1,19 @@
 
+using System;
+
 namespace RL_API{
-    public class TileInfo(int tileType, int liquidType, int liquidAmount)
+    public class TileInfo(int tileType, int liquidType, int liquidAmount, float brightness)
     {
         private int tileType = tileType;
         private int liquidType = liquidType;
         private int liquidAmount = liquidAmount;
+        private float brightness = brightness;
 
         public byte TileType { get; internal set; }
+
+        internal double getBrightness()
+        {
+            return Math.Round(brightness,2);
+        }
     }
 }
