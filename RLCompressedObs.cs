fd06c302b557112b4c4141372bc966a78fa1369c
Felipe <feliperkj805@gmail.com>
2025-04-27 17:16:36 -0400

    Changed all types to float and added Flatten() function

--- RLCompressedObs.cs
+++ RLCompressedObs.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace RL_API
 {
     public class RLCompressedObs
@@ -5,17 +8,54 @@ namespace RL_API
         public float[] NearItems { get; set; }
         public float[] InvState { get; set; }
         public float[] PlayerInfo { get; set; }    // [VelocityX, VelocityY, PosX, PosY, HealthRatio, ManaRatio]
-        public byte[] TileTypes { get; set; }       // Flattened 1D array of tile types around the player
-        public byte HeldItemType { get; set; }
-        public byte NearbyEnemies { get; set; }
-        public sbyte FacingDirection { get; set; }  // -1 = left, 1 = right
-        public byte TotalDefense { get; set; }
-        public byte[] BuffIds { get; set; }
-        public byte[] AccessoryIds { get; set; }
-        public byte CurrentBiome { get; set; } 
-        public byte HookType { get; set; }
-        public byte MountType { get; set; }
-        public byte LightPetType { get; set; }
-        public byte PetType { get; set; }     // Single biome ID (for now, or most important one)
+        public float[] TilesAround { get; set; }       // Flattened 1D array of tile types around the player
+        public float HeldItemType { get; set; }
+        public float NearbyEnemies { get; set; }
+        public float FacingDirection { get; set; }  // -1 = left, 1 = right
+        public float TotalDefense { get; set; }
+        public float[] BuffIds { get; set; }
+        public float[] AccessoryIds { get; set; }
+        public float CurrentBiome { get; set; } 
+        public float HookType { get; set; }
+        public float MountType { get; set; }
+        public float LightPetType { get; set; }
+        public float PetType { get; set; }     // Single biome ID (for now, or most important one)
+        public float[] Flatten()
+        {
+            var floats = new List<float>();
+
+            if (NearItems != null)
+                floats.AddRange(NearItems);
+
+            if (InvState != null)
+                floats.AddRange(InvState);
+
+            if (PlayerInfo != null)
+                floats.AddRange(PlayerInfo);
+
+            if (TilesAround != null)
+                floats.AddRange(Array.ConvertAll(TilesAround, b => (float)b));
+
+            floats.Add((float)HeldItemType);
+            floats.Add((float)NearbyEnemies);
+            floats.Add((float)FacingDirection);
+            floats.Add((float)TotalDefense);
+
+            if (BuffIds != null)
+                floats.AddRange(Array.ConvertAll(BuffIds, b => (float)b));
+
+            if (AccessoryIds != null)
+                floats.AddRange(Array.ConvertAll(AccessoryIds, b => (float)b));
+
+            floats.Add((float)CurrentBiome);
+            floats.Add((float)HookType);
+            floats.Add((float)MountType);
+            floats.Add((float)LightPetType);
+            floats.Add((float)PetType);
+
+            return floats.ToArray();
+        }
+
     }
+    
 }
