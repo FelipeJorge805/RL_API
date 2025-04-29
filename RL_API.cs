78f3fd016ddb686e86d319d417f4b26324cbdd5c
FelipeJorge805 <feliperkj805@gmail.com>
2025-04-28 22:03:06 -0400

    Added checks for --agentX command line and auto connection to server

--- RL_API.cs
+++ RL_API.cs
@@ -1,22 +1,101 @@
+using System.IO;
+using System.Text.Json;
+using Terraria;
 using Terraria.ModLoader;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
 
 namespace RL_API
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class RL_API : Mod
-	{
-		public override void Load()
+    public class RL_API : Mod
+    {
+        private static bool LoopHasBeenCalled = false;
+        public override void Load()
         {
-			base.Load();
+            bool isAgent = Environment.GetCommandLineArgs().Any(arg => arg.Contains("--agent"));
+            if (!isAgent && !LoopHasBeenCalled)
+            {
+                Logger.Info("\n\nNon Agent mode detected, skipping join loop.\n");
+                return;
+            }else{
+                StartJoinLoop();
+                LoopHasBeenCalled = true;
+                Logger.Info("\n\nAgent mode detected, starting join loop.\n");
+            }
         }
-        /*public void saveLog(){
-			Logger.Info(playerPos);
-			ModContent.GetInstance<RL_API>().Logger.Info("This is a log message.");
-		}*/
+
+        private async void StartJoinLoop()
+        {
+            
+            
+            string configPath;
+            try{
+                configPath = Path.Combine("D:/SteamLibrary/steamapps/common/Tmods", "client_config.json");
+                if (!File.Exists(configPath))
+                {
+                    Logger.Warn("No client_config.json found, skipping.");
+                    Logger.Warn("Path: "+Main.SavePath);
+                    return;
+                }
+            }catch (System.Exception ex)
+            {
+                Logger.Error($"Error loading config: {ex}");
+                return;
+            }
+
+            var json = File.ReadAllText(configPath);
+            var config = JsonSerializer.Deserialize<ClientConfig>(json);
+
+            Logger.Info("Waiting for menu to join...");
+			Logger.Info($"Joining {config.server_ip}:{config.server_port}");
+
+            while (true)
+            {
+				Logger.Info($"Joining {config.server_ip}:{config.server_port}");
+
+                await Task.Delay(10_000);
+
+                if (Main.gameMenu)
+                {
+                    try
+                    {
+                        Logger.Info($"Joining {config.server_ip}:{config.server_port}");
+
+                        Netplay.ServerIP = System.Net.IPAddress.Parse("127.0.0.1");        // string to IPAddress
+                        //Netplay.serverServerPort = config.server_port;    // int
+                        Netplay.StartTcpClient();                  // trigger connection
+                        Main.autoJoin = true;
+
+                        ConnectionManager.Initialize(config.server_ip, config.agent_port);
+
+                        Logger.Info("Started join + socket.");
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Logger.Error($"Error starting join: {ex}");
+                    }
+
+                    break;
+                }
+                else
+                {
+                    Logger.Info("Not in main menu yet, waiting...");
+                }
+            }
+        }
+
         public override void Unload()
         {
             base.Unload();
-			ConnectionManager.Close();
+            ConnectionManager.Close();
         }
-	}
+
+        private class ClientConfig
+        {
+            public string server_ip { get; set; }
+            public int server_port { get; set; }
+            public int agent_port { get; set; }
+        }
+    }
 }
