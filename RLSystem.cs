b8afed47a88b49598f797f8e8d1bd05c47944f82
Felipe <feliperkj805@gmail.com>
2025-05-01 16:55:38 -0400

    Added disable useless settings functions, makes the game look like a ghost

--- RLSystem.cs
+++ RLSystem.cs
@@ -9,14 +9,20 @@ namespace RL_API
 {
     public class RLSystem : ModSystem
     {
+        public override void Load()
+        {
+            DisableUselessSettings();
+        }
+
         public override void OnWorldLoad()
         {
             base.OnWorldLoad();
+            DisableUselessSettings();
             try{
                 string[] args = Environment.GetCommandLineArgs();
                 var idPort = 0;
                 foreach(string arg in args){
-                    Main.NewText(arg);
+                    //Main.NewText(arg);
                     if(arg.Contains("--agent")) int.TryParse(arg.Split('=')[1], out idPort);
                 }
                 ConnectionManager.Initialize("127.0.0.1", 5000+idPort);
@@ -25,10 +31,39 @@ namespace RL_API
                 Main.NewText("Socket Connection Error: "+e);
             }
         }
+        public override void PreUpdateEntities()
+        {
+            Main.hasFocus = true;
+            DisableUselessSettings();
+        }
         public override void OnWorldUnload()
         {
             base.OnWorldUnload();
             ConnectionManager.Close();
+        }
+        private void DisableUselessSettings()
+        {
+            // Audio
+            Main.soundVolume = 0f;
+            Main.musicVolume = 0f;
+
+            // Lighting
+            Lighting.Mode = Terraria.Graphics.Light.LightMode.White; // 0 = white, 1 = retro, 2 = color
+            Lighting.GlobalBrightness = 1f;
+
+            // Drawing
+            Main.drawSkip = true; // May not work in all tML versions
+            Main.renderCount = 0;
+            Main.drawToScreen = false;
+
+            // Disable fancy extras
+            Main.maxQ = false; // Older version cloud draw cap
+            Main.cloudAlpha = 0f;
+            Main.windSpeedCurrent = 0f;
+            Main.windSpeedTarget = 0f;
+
+            Main.render = false;
+            Main.skipMenu = true; // Stops menu animations
         }
     }
 }
